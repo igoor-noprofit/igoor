dc035ab8099611ff1005130a94cc015b189ff08c
Carlo <admin@igoor.org>
2025-01-14 16:04:46 +0100

    bat with WATCHDOG

--- igoor.bat
+++ igoor.bat
@@ -1,15 +1,21 @@
 @echo on
-echo Starting IGOOR...
+echo Starting IGOOR watchdog...
 echo Activating virtual environment...
 call venv\scripts\activate
+
+:loop
 echo Setting environment variables...
 set "IGOOR_FULLSCREEN=True"
-set "IGOOR_DEBUG=False"
+set "IGOOR_DEBUG=True"
 set "IGOOR_ONTOP=True"
-echo Starting application...
 
+echo Starting application...
 :: Launch PowerShell to minimize the window after a brief delay
 start /B powershell -window minimized -Command "Start-Sleep -Seconds 2; (Get-Process -Id $PID).MainWindowHandle | ForEach-Object {$win32ShowWindow::ShowWindow($_, 6)}"
 
 :: Start the Python application
 python main.py
+
+echo Application stopped or crashed. Restarting in 5 seconds...
+timeout /t 5 /nobreak
+goto loop
