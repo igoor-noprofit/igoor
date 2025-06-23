ddf64628467d08af22ee460e1843312d799af1c7
Carlo <admin@igoor.org>
2025-06-23 12:33:13 +0200

    BUGFIX

--- plugins/shortcuts/frontend/shortcuts_component.vue
+++ plugins/shortcuts/frontend/shortcuts_component.vue
@@ -2,17 +2,17 @@
     <div class="shortcuts shortcuts-plugin">
         <button class="btn btn-shortcut" @click="$_minimise()">
             <img src="img/minimize.svg">
-            <h3>{{translations['Minimize']}}</h3>
+            <h3>{{ translations['Minimize'] }}</h3>
         </button>
         <button class="btn btn-shortcut" @click="$_speak('J\'ai soif')"><svg class="icon icon-l">
                 <use xlink:href="img/svgdefs.svg#icon-drink"></use>
             </svg>
-            <h3>{{translations['Drink']}}</h3>
+            <h3>{{ translations['Drink'] }}</h3>
         </button>
         <button class="btn btn-shortcut" @click="$_speak('J\'ai besoin d\'aller au toilette')"><svg class="icon icon-l">
                 <use xlink:href="img/svgdefs.svg#icon-toilet"></use>
             </svg>
-            <h3>{{translations['Toilet']}}</h3>
+            <h3>{{ translations['Toilet'] }}</h3>
         </button>
         <button class="btn btn-shortcut" @click="$_parole()"><svg class="icon icon-l">
                 <use xlink:href="img/svgdefs.svg#icon-talk"></use>
@@ -22,22 +22,23 @@
         <button class="btn btn-shortcut" @click="$_speak('Oui')"><svg class="icon icon-l">
                 <use xlink:href="img/svgdefs.svg#icon-ok"></use>
             </svg>
-            <h3>{{translations['Yes']}}</h3>
+            <h3>{{ translations['Yes'] }}</h3>
         </button>
         <button class="btn btn-shortcut" @click="$_speak('Non')"><svg class="icon icon-l">
                 <use xlink:href="img/svgdefs.svg#icon-no"></use>
             </svg>
-            <h3>{{translations['No']}}</h3>
+            <h3>{{ translations['No'] }}</h3>
         </button>
         <button class="btn btn-shortcut" @click="$_speak('Merci')"><svg class="icon icon-l">
                 <use xlink:href="img/svgdefs.svg#icon-thankyou"></use>
             </svg>
-            <h3>{{translations['Thanks']}}</h3>
+            <h3>{{ translations['Thanks'] }}</h3>
         </button>
-        <button class="btn btn-shortcut btn-hilite" @click="$_speak('Aidez-moi c\'est urgent!')"><svg class="icon icon-l">
+        <button class="btn btn-shortcut btn-hilite" @click="$_speak('Aidez-moi c\'est urgent!')"><svg
+                class="icon icon-l">
                 <use xlink:href="img/svgdefs.svg#icon-sos"></use>
             </svg>
-            <h3>{{translations['Help!']}}</h3>
+            <h3>{{ translations['Help!'] }}</h3>
         </button>
     </div>
 </template>
@@ -49,15 +50,22 @@ export default {
     data() {
         return {
             websocket: null,  // Store WebSocket instance
-            status: 'loading',
-            paroles: [
-                translations["I'm on my way, just finishing writing"],
-                translations["Just a moment, I'll finish"],
-                translations["One second and it's done"],
-                translations["Just give me two seconds"],
-                translations["I'm just finishing"]
-            ]
+            status: 'loading'
         };
+    },
+    computed: {
+        paroles() {
+            if (!this.translations || Object.keys(this.translations).length === 0) {
+                return [];
+            }
+            return [
+                this.translations["I'm on my way, just finishing writing"],
+                this.translations["Just a moment, I'll finish"],
+                this.translations["One second and it's done"],
+                this.translations["Just give me two seconds"],
+                this.translations["I'm just finishing"]
+            ];
+        }
     },
     methods: {
         $_speak(msg) {
@@ -66,10 +74,10 @@ export default {
             console.log(json);
             this.sendMsgToBackend(json);
         },
-        $_minimise(){
+        $_minimise() {
             window.pywebview.api.win_minimize()
         },
-        $_parole(){
+        $_parole() {
             const randomIndex = Math.floor(Math.random() * this.paroles.length);
             const randomMsg = this.paroles[randomIndex];
             this.$_speak(randomMsg);
@@ -109,7 +117,8 @@ export default {
 
 
 .btn-shortcut h3 {
-    margin: 4px 0 0 0;  /* Slightly increased top margin */
+    margin: 4px 0 0 0;
+    /* Slightly increased top margin */
     font-size: 0.8em;
     white-space: nowrap;
 }
