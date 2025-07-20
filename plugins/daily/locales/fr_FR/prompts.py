bd50b70c120524cfb28080a9a9ba7bfb3e8e659d
Carlo <admin@igoor.org>
2025-07-20 10:54:05 +0200

    more examples for daily prompt

--- plugins/daily/locales/fr_FR/prompts.py
+++ plugins/daily/locales/fr_FR/prompts.py
@@ -16,17 +16,35 @@ Tu recevras aussi un JSON avec des infos du contexte dynamique, ex.:
 Dans l'exemple, il est probable que la personne en situation d'handicap souhaite savoir si quelqu'un peut aller faire des courses,parce qu'il commence à être tard pour le déjeuner. 
 Propose de 3 à 5 phrases qui tiennent compte en premier lieu du thème, ainsi que des handicaps de la personne; mais aussi des tags proposés et,si pertinentes,des informations venant du contexte (mémoire à court terme, à long terme etc). 
 Pars des situations les plus probables au quotidien.
-Le résultat doit être obligatoirement un tableau JSON dans le format suivant : 
+Le résultat doit être obligatoirement un objet JSON avec un champ "answers" contenant une liste de phrase.
+Retourne uniquement un JSON, sans guillemets, sans balises Markdown, sans explication.
 </instructions>
 
-<example>
+<examples>
+Entrée :
+"catégorie":"nourriture","thème":"courses","tags": ["quand"] {"lieu_actuel":"maison","heure":"11:36"}
+
+Sortie: 
 {"answers":["Il faut qu'on se dépêche pour les courses, non ?","Est-ce qu'on a quelque chose à manger pour le déj ?", "Tu peux aller faire des courses ? Il n'y a plus grand-chose dans le frigo"]} 
-</example>
+
+Entrée :
+"catégorie":"inconforts","thème":"dossier lit","tags": ""
+
+Sortie:
+{"answers": ["Tu peux remonter le dossier du lit ?", "Tu peux baisser le dossier du lit ?"]}
+
+Entrée :
+"catégorie":"inconforts","thème":"mains","tags": ""
+
+Sortie:
+{"answers": ["Mes mains me font mal, tu peux les eloigner ?", "Tu peux rapprocher mes mains ?","Tu peux me masser les mains ?"]}
+</examples>
 
 <instructions>
 Utilise un langage familier et des réponses courtes, en préférant celles liées aux situations quotidiennes et aux temps les plus proches, le tutoiement au vouvoiement.
 NE METS PAS les phrases à la troisième personne et centre le discours sur la personne affectée par la maladie.
-N'explique jamais ta réponse, retourne exclusivement le JSON valide.
+N'explique JAMAIS ta réponse, retourne EXCLUSIVEMENT le JSON valide.
+
 </instructions>
 """,
         "usr": """<context>
