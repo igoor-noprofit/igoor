4f1c11822f5ed590fa76bdc3cbf27d52beeccc16
Carlo <admin@igoor.org>
2025-09-29 17:32:53 +0200

    working update settings

--- plugins/ttsdefault/ttsdefault.py
+++ plugins/ttsdefault/ttsdefault.py
@@ -11,19 +11,24 @@ class Ttsdefault(Baseplugin):
         self.settings = self.get_my_settings()
         # Initialize SAPI
         self.fallback_only = self.settings.get("fallback_only")
-        self.speaker_voice = self.settings.get("speaker_voice")
+        self.voice_id = self.settings.get("voice_id")
         try:
             self.speaker = win32com.client.Dispatch("SAPI.SpVoice")
             voices = self.speaker.GetVoices()
             self.available_voices = []
             self.logger.info("AVAILABLE VOICES:")
-            for voice in voices:
+            for index, voice in enumerate(voices):
                 description = voice.GetDescription()
                 self.logger.info(f"- {description}")
                 parts = description.split(' - ', 1)
-                voice_id = parts[0]
                 lang = parts[1] if len(parts) > 1 else ''
-                self.available_voices.append({"voice_id": voice_id, "lang": lang})
+                self.available_voices.append({
+                    "voice_id": index,
+                    "voice_label": description,
+                    "lang": lang
+                })
+
+                print(self.available_voices)
             
             self.is_loaded = True
             self.update_my_settings("voice_list", self.available_voices)
@@ -35,11 +40,20 @@ class Ttsdefault(Baseplugin):
     def startup(self):
         if self.is_loaded:
             try:
-                self.speaker.Voice = self.speaker.GetVoices().Item(self.speaker_voice)
+                self.speaker.Voice = self.speaker.GetVoices().Item(self.voice_id)
             except Exception as e:
-                print(f"Error occurred while setting voice to : {self.speaker_voice}")
+                print(f"Error occurred while setting voice to : {self.voice_id}")
                 self.speaker.Voice = self.speaker.GetVoices().Item(0)
                 return False   
+    
+    '''
+    @hookimpl
+    def test_speak(self, message, **kwargs):
+        self.get_my_settings()
+        voice_id = kwargs.get('voice_id', self.voice_id)
+        print(f"TEST SPEAK with pitch={pitch}, rate={rate}, volume={volume}")
+        self.speak(message)
+    '''
         
     @hookimpl
     def speak(self, message):
