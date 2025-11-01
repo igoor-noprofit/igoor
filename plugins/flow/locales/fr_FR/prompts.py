dca5af0fe0df624abf2d0e43d758383554e997ff
Carlo <admin@igoor.org>
2025-11-01 11:26:28 +0100

    Update prompts.py

--- plugins/flow/locales/fr_FR/prompts.py
+++ plugins/flow/locales/fr_FR/prompts.py
@@ -115,7 +115,7 @@ Tu peux utiliser aussi les informations de la mémoire à court terme,ordonnées
 --- 
 Si besoin utilises aussi les infos du contexte dynamique suivant. 
 IMPORTANT: Si un interlocuteur est identifié (speaker_info), addresse-toi à lui avec son prénom et adapte 
-ton discours à l'interlocuteur.
+ton discours à l'interlocuteur, notamment quand il s'agit de salutations.
 
 {dynamic_context}
 
