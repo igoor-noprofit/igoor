71c019fcf901b821926b8e83254389755cedfb0b
Carlo <admin@igoor.org>
2026-02-03 15:45:09 +0100

    BUGFIX for version number

--- get_version.ps1
+++ get_version.ps1
@@ -1,6 +1,14 @@
-$content = Get-Content "version.py"
-$match = $content | Select-String "__version__"
-if ($match) {
-    $version = ($match -split "=")[1].Trim().Replace("`"", "").Replace("'", "").Trim()
-    Write-Output $version
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$versionFilePath = Join-Path $scriptPath "version.py"
+
+if (Test-Path $versionFilePath) {
+    $content = Get-Content $versionFilePath
+    $match = $content | Select-String "__version__"
+    if ($match) {
+        $version = ($match -split "=")[1].Trim().Replace("`"", "").Replace("'", "").Trim()
+        $version = $version.Trim()
+        $Host.UI.WriteErrorLine($version)
+    }
+} else {
+    Write-Error "version.py not found at: $versionFilePath"
 }
